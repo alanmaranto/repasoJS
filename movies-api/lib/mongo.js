fd5c2314053fb67057f89b2ccb8edd7a95f954b4
alanmaranto <alanarostegui@hotmail.com>
2019-11-11 20:50:17 -0600

    conectando crud con mongodb

--- movies-api/lib/mongo.js
+++ movies-api/lib/mongo.js
@@ -5,7 +5,7 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.port}/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
     constructor(){
@@ -26,6 +26,42 @@ class MongoLib {
             })
         }
         return MongoLib.connection;
+    }
+
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db
+            .collection(collection)
+            .find(query)
+            .toArray()
+        });
+    }
+
+    get(collection, id){
+        return this.connect().then(db => {
+            return db.connection(collection).findOne({ _id: ObjectId(id) });
+        });
+    }
+
+    create(collection, data){
+        return this.connect().then(db => {
+            return db.collection(collection).insertOne(data);
+        })
+        .then(result => result.insertId);
+    }
+
+    update(collection, data, id){
+        return this.connect().then(db => {
+            return db.collection(collection).updateOne({ _id: ObjectId(id)}, { $set: data}, { upsert: true });
+        })
+        .then(result => result.upsertedId || id);
+    }
+
+    delete(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).deleteOne({ _id: ObjectId(id )});
+        })
+        .then(() => id)
     }
 }
 
--- movies-api/services/movies.js
+++ movies-api/services/movies.js
@@ -1,28 +1,32 @@
-const { moviesMock } = require('../utils/mocks/movies');
-
+const MongoLib= require('../lib/mongo');
 class MoviesService {
-    async getMovies(){
-        const movies = await Promise.resolve(moviesMock);
+    constructor(){
+        this.collection = 'movies';
+        this.mongoDB = new MongoLib();
+    }
+    async getMovies({ tags }){
+        const query = tags && { tags: { $in: tags }}
+        const movies = await this.mongoDB.getAll(this.collection, query);
         return movies || [];
     }
 
-    async getMovie() {
-        const movie = await Promise.resolve(moviesMock[0]);
+    async getMovie({ movieId }) {
+        const movie = await this.mongoDB.get(this.collection, movieId);
         return movie || {};
     }
 
-    async createMovie(){
-        const createMovieId = await Promise.resolve(moviesMock[0].id);
+    async createMovie({ movie }){
+        const createMovieId = await this.mongoDB.create(this.collection, movie);
         return createMovieId;
     }
 
-    async updateMovie(){
-        const updateMovieId = await Promise.resolve(moviesMock[0].id);
+    async updateMovie({ movieId, movie }){
+        const updateMovieId = await this.mongoDB.update(this.collection, movieId, movie);
         return updateMovieId
     }
 
-    async deleteMovie(){
-        const deleteMovieId = await Promise.resolve(moviesMock[0].id);
+    async deleteMovie({ movieId }){
+        const deleteMovieId = await this.mongoDB.delete(this.collection, movieId );
         return deleteMovieId
     }
 }
